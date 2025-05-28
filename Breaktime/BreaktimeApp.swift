69d35634425badc8c064971ef435d85801fdcc96
Nathan Chan <nathan3108chan@gmail.com>
2025-05-29 00:46:31 +1000

    Initial Commit

---
+++ Breaktime/BreaktimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreaktimeApp.swift
+//  Breaktime
+//
+//  Created by Nathan Chan on 29/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct BreaktimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BreaktimeTests/BreaktimeTests.swift
@@ -0,0 +1,17 @@
+//
+//  BreaktimeTests.swift
+//  BreaktimeTests
+//
+//  Created by Nathan Chan on 29/5/2025.
+//
+
+import Testing
+@testable import Breaktime
+
+struct BreaktimeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
